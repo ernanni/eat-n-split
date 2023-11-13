c5e5c68f0d19ed4f6ab8da401948173108ac0c41
Ernanni <ernannicf@gmail.com>
2023-11-13 05:09:51 -0300

    project scaffolding

---
+++ src/components/FriendsList/index.js
@@ -0,0 +1,11 @@
+import Friend from '../Friend';
+
+export default function FriendsList({ friends }) {
+  return (
+    <ul>
+      {friends.map((friend) => (
+        <Friend key={friend.id} friend={friend} />
+      ))}
+    </ul>
+  );
+}
---
+++ src/components/index.js
@@ -0,0 +1,7 @@
+import Button from './Button';
+import FormAddFriend from './FormAddFriend';
+import FormSplitBill from './FormSplitBill';
+import Friend from './Friend';
+import FriendsList from './FriendsList';
+
+export { Button, FormAddFriend, FormSplitBill, Friend, FriendsList };
